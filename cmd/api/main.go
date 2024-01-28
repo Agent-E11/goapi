cf709553bffd0583edefc0e25c105c0f7e984d19
Eamon (WSL Laptop) <eamon.r.burns@gmail.com>
2024-01-27 16:04:17 -0800

    Finished tutorial

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/go-chi/chi"
-    "github.com/agent-e11/goapi/internal/handlers"
-    log "github.com/sirupsen/logrus"
+	"github.com/agent-e11/goapi/internal/handlers"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
 )
+
+func main() {
+    log.SetReportCaller(true)
+    var r *chi.Mux = chi.NewRouter()
+
+    handlers.Handler(r)
+
+    fmt.Println("Starting GO API Service...")
+
+    err := http.ListenAndServe("localhost:8000", r)
+    if err != nil {
+        log.Error(err)
+    }
+}
